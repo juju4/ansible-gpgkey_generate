77bfbc42b57118c1eed3868c4ceb272c3b940f7b
Your Name <you@example.com>
2019-01-04 15:02:56 +0000

    galaxy dependency update

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -5,11 +5,10 @@ umask 022
 
 rolesdir=$(dirname $0)/..
 
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.gpgkey_generate ] && ln -s ansible-gpgkey_generate $rolesdir/juju4.gpgkey_generate
 [ ! -e $rolesdir/juju4.gpgkey_generate ] && cp -R $rolesdir/ansible-gpgkey_generate $rolesdir/juju4.gpgkey_generate
 
 ## don't stop build on this script return code
 true
-
